e4509119e741a6f316d31baac9348817e106698d
fernando <jflisazo@hotmail.com>
2024-05-19 11:24:07 -0300

    agregado numero de intentos en acertijo.py

--- acertijo.py
+++ acertijo.py
@@ -2,15 +2,15 @@ import random
 
 acertijo = random.randrange(0,100,1)
 numero = -1
-
+cont = 0
 while acertijo != numero:
     print("Dame un numero entre 0 y 100: ")
     numero = int(input())
-
+    cont = cont + 1
     if numero < acertijo:
         print("Muy chico!!!")
     elif numero > acertijo:
         print("Muy grande!!!")
     else:
         print("Acertaste, eres un genio!!!")
-
+        print ("Numero de intentos: ", cont)
